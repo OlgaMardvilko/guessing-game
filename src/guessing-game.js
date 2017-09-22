2e643fd35cba7a4f102770239161968c457ec001
vitol <omardvilko@mail.ru>
2017-09-22 04:26:32 +0300

    my solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {   		//функция constructor запускается при создании new GuessingGame
+		this.min = 0;
+		this.max = 0;
+		this.candidateNumber = 0;
+	}
+							// методы, которые записываются в GuessingGame.prototype
+    setRange(min, max) {	// принимаем минимальные и максимальные значения диапазона для угадывания
+		this.min = min;
+		this.max = max;
     }
 
-    guess() {
-
+    guess() {  				// предполагаем кандидата и возвращаем значение предполагаемого кандидата 
+		this.candidateNumber = Math.floor((this.min+this.max)/2);	// для предположения используем алгоритм бинарного поиска
+		return this.candidateNumber;
     }
 
-    lower() {
-
+    lower() {				// если return this.candidateNumber больше чем ответ, то далее для предположения максимальное значение соответсвует кандидату
+		this.max = this.candidateNumber;
     }
 
-    greater() {
-
+    greater() {				// если return this.candidateNumber меньше чем ответ, то далее для предположения минимальное значение соответсвует кандидату
+		this.min = this.candidateNumber;
     }
 }
 
